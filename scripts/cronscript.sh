5979238b0fc496d612bfa6d705b360917c1f05a8
kf-cuanschutz <127144640+kf-cuanschutz@users.noreply.github.com>
2024-06-18 15:35:06 -0600

    Update cronscript.sh

--- scripts/cronscript.sh
+++ scripts/cronscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-0 4 * * 1-5 /projects/kfotso@xsede.org/slurm_report/June_2024_/alpine_reporting_tools/scripts/actual_script USERS=brunetti@xsede.org,kfotso@xsede.org,msherren@xsede.org START_DATE=$YEAR-$previousMonth-$Format_day_week_ago END_DATE=$TODAY
+0 4 * * 1-5 /projects/kfotso@xsede.org/slurm_report/June_2024_/alpine_reporting_tools/scripts/actual_script.sh 

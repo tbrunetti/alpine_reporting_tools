412ab0b35dfe0d3180daeb1fe96c1307e1f47867
MSherren <maclean.sherren@cuanschutz.edu>
2024-07-22 15:49:59 -0600

    Update get_startandsubmit_time.sh

--- scripts/get_startandsubmit_time.sh
+++ scripts/get_startandsubmit_time.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-output=$(sacct --allusers --starttime=05/01/24 --endtime=05/02/24 -X --format
-=JobID,Account,start,submit | grep -i amc)
+output=$(sacct --allusers --starttime=05/01/24 --endtime=05/02/24 -X --format=JobID,Account,start,submit | grep $
 
 # echo output
 
 # Extract start and submit times
-echo "$output" | awk '{print $3,$4}' # > output.csv # test first
+echo "$output" | awk '{print $3,$4}' > submitstart.csv
 
+
+# FUTURE: potentially call ml anaconda and conda env
 # FUTURE: Call python script that calculates the difference between start and submit:
-# example.py
-
+python queue_time_calc.py

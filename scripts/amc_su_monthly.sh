9d96f633927677c383061dd1e62b644e29f9724c
MSherren <maclean.sherren@cuanschutz.edu>
2024-07-22 14:19:37 -0600

    Update and clean amc_su_monthly.sh

--- scripts/amc_su_monthly.sh
+++ scripts/amc_su_monthly.sh
@@ -11,14 +11,3 @@ output=$(sreport -t hours -T billing -P cluster AccountUtilizationByUser \
 # Process output to extract numbers and calculate total
 echo "$output" | awk '{print $NF}' | grep -Eo '[0-9]+' | awk '{sum += $1} END {print "Total Monthly
  SUs used by AMC Users: ", sum}'
-
-
-
-#result=$(echo "$output" | awk '{gsub(/\|billing\|/, " "); print $3, $5; sum += $5} END {print "Tot
-al: ", sum}')
-#result=$(echo "$output" | awk '{print $3, gensub(/.*\|/, "", "g", $6); sum += gensub(/.*\|/, "", "
-g", $6)} END {print "Total: ", sum}')
-# Print the results for 1 month out, keeping only the name and SUs used. Also make it print a sum t
-otal
-#result=$(echo "$output" | awk '{print $3, substr($6, index($6, "|")+1); sum += substr($5, index($5
-, "|")+1)} END {print "Total Monthly SUs: ", sum}')

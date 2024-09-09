2770b0b1b2b1adb53df50df4b648a5878110e930
kf-cuanschutz <127144640+kf-cuanschutz@users.noreply.github.com>
2024-09-09 15:02:00 -0600

    Fixed .py scripts

--- scripts/parse_eff_+_df.py
+++ scripts/parse_eff_+_df.py
@@ -25,10 +25,10 @@ d = {'Job ID': [],
      'User': [],
      'State': [],
      'Cores': [],
-     'CPU Efficiency': [],
+     'CPU Efficiency (%)': [],
      'Walltime': [],
      'Memory Utilized': [],
-     'Memory Efficiency': [],
+     'Memory Efficiency (%)': [],
      } 
 
 user_list = []
@@ -72,7 +72,7 @@ for line in data: # go through file line by line
         key, val = line.split(':', 1)
         eff, text = val.split('%',1)
         val = float(eff)
-        d['CPU Efficiency'].append(eff)
+        d['CPU Efficiency (%)'].append(eff)
         
     elif line.startswith('Job Wall-clock time'):
         line = line.replace('\n', '') # get rid of '\n' in all fields
@@ -107,7 +107,7 @@ for line in data: # go through file line by line
         key, val = line.split(':', 1)
         mem_eff, text = val.split('%',1)
         val = float(mem_eff)
-        d['Memory Efficiency'].append(mem_eff)
+        d['Memory Efficiency (%)'].append(mem_eff)
 
 df_eff = pd.DataFrame(d,)
 
@@ -119,71 +119,32 @@ print(f"********Creating and saving individual dataframes for each 5 users******
 
 # Creating individual dataframes for each 5 users:
 #
+user_list_df_each_user = []
 for ind,val in enumerate(user_list):
     mask = df_eff["User"] == val   
     exec(f'df_user_{ind}= df_eff[mask]')
   
     # Saving individual dataframes:
     exec(f'df_user_{ind}.to_excel("Eff_top_{val}_user.xlsx")')
+    
+    # Storing each individual df to the list
+    exec(f'user_list_df_each_user.append(df_user_{ind})')
 
 
 # Reformatting the CPU eff and Mem eff data
-df_user_0["CPU Efficiency"]= pd.to_numeric(df_user_0["CPU Efficiency"])
-df_user_1["CPU Efficiency"]= pd.to_numeric(df_user_1["CPU Efficiency"])
-df_user_2["CPU Efficiency"]= pd.to_numeric(df_user_2["CPU Efficiency"])
-df_user_3["CPU Efficiency"]= pd.to_numeric(df_user_3["CPU Efficiency"])
-df_user_4["CPU Efficiency"]= pd.to_numeric(df_user_4["CPU Efficiency"])
 
-df_user_0["Memory Efficiency"]= pd.to_numeric(df_user_0["Memory Efficiency"])
-df_user_1["Memory Efficiency"]= pd.to_numeric(df_user_1["Memory Efficiency"])
-df_user_2["Memory Efficiency"]= pd.to_numeric(df_user_2["Memory Efficiency"])
-df_user_3["Memory Efficiency"]= pd.to_numeric(df_user_3["Memory Efficiency"])
-df_user_4["Memory Efficiency"]= pd.to_numeric(df_user_4["Memory Efficiency"])
-
-# Plotting and saving the plots for CPU eff
-sns.catplot(data=df_user_0, x="State", y="CPU Efficiency",  kind="box")
-cpu_efficiency_user = "CPU_eff_user_0.png"
-plt.savefig(cpu_efficiency_user)
-
-# Plotting and saving the plots for Mem eff
-sns.catplot(data=df_user_0, x="State", y="Memory Efficiency",  kind="box")
-mem_efficiency_user = "Mem_eff_user_0.png"
-plt.savefig(mem_efficiency_user)
-
-
-# Plotting and saving the plots for CPU eff
-sns.catplot(data=df_user_1, x="State", y="CPU Efficiency",  kind="box")
-cpu_efficiency_user = "CPU_eff_user_1.png"
-plt.savefig(cpu_efficiency_user)
-# Plotting and saving the plots for Mem eff
-sns.catplot(data=df_user_1, x="State", y="Memory Efficiency",  kind="box")
-mem_efficiency_user = "Mem_eff_user_1.png"
-plt.savefig(mem_efficiency_user)
-
-# Plotting and saving the plots for CPU eff
-sns.catplot(data=df_user_2, x="State", y="CPU Efficiency",  kind="box")
-cpu_efficiency_user = "CPU_eff_user_2.png"
-plt.savefig(cpu_efficiency_user)
-# Plotting and saving the plots for Mem eff
-sns.catplot(data=df_user_2, x="State", y="Memory Efficiency",  kind="box")
-mem_efficiency_user = "Mem_eff_user_2.png"
-plt.savefig(mem_efficiency_user)
-
-
-# Plotting and saving the plots for CPU eff
-sns.catplot(data=df_user_3, x="State", y="CPU Efficiency",  kind="box")
-cpu_efficiency_user = "CPU_eff_user_3.png"
-plt.savefig(cpu_efficiency_user)
-# Plotting and saving the plots for Mem eff
-sns.catplot(data=df_user_3, x="State", y="Memory Efficiency",  kind="box")
-mem_efficiency_user = "Mem_eff_user_3.png"
-plt.savefig(mem_efficiency_user)
-
-# Plotting and saving the plots for CPU eff
-sns.catplot(data=df_user_4, x="State", y="CPU Efficiency",  kind="box")
-cpu_efficiency_user = "CPU_eff_user_4.png"
-plt.savefig(cpu_efficiency_user)
-# Plotting and saving the plots for Mem eff
-sns.catplot(data=df_user_4, x="State", y="Memory Efficiency",  kind="box")
-mem_efficiency_user = "Mem_eff_user_4.png"
-plt.savefig(mem_efficiency_user)
+for ind,val in enumerate(user_list):
+    user_list_df_each_user[ind]["CPU Efficiency (%)"] = user_list_df_each_user[ind]["CPU Efficiency (%)"].astype(float)
+    user_list_df_each_user[ind]["Memory Efficiency (%)"] = user_list_df_each_user[ind]["Memory Efficiency (%)"].astype(float)
+    
+    print(f"********Plotting and saving the plots for CPU eff********\n")
+    sns.catplot(data=user_list_df_each_user[ind], x="State", y="CPU Efficiency (%)",  kind="box")
+    cpu_efficiency_user=f"CPU_eff_user_{val}_.png"
+    plt.title("CPU efficiency (%)")
+    plt.savefig(cpu_efficiency_user)
+    
+    print(f"********Plotting and saving the plots for Mem eff********\n")
+    sns.catplot(data=user_list_df_each_user[ind], x="State", y="Memory Efficiency (%)",  kind="box")
+    mem_efficiency_user=f"Mem_eff_user_{val}_.png"
+    plt.title("Mem efficiency (%)")
+    plt.savefig(mem_efficiency_user)
--- scripts/plot_levelfs_SU.py
+++ scripts/plot_levelfs_SU.py
@@ -12,15 +12,17 @@ with open(levelfs, 'r') as f:
     data = f.readlines()
     
 #d = {'User': [], 'User_levelfs': [], 'Inst_levelfs': [], }
-d = {'User_levelfs': [], 'Inst_levelfs': [], } 
-index = []
+d = { 'Username': [],
+        'User_levelfs': [], 'Inst_levelfs': [], } 
+
+index_levelfs = []
 for line in data: # go through file line by line      
     if line.startswith('NEW USER'): #or line != 'LEVELFS\n': # skip new line characters
         line = line.replace('\n', '') # get rid of '\n' in all fields
         key, val = line.split(':', 1) # take the first 2 tokens from the split statement
         
         if key == 'NEW USER':
-            index.append(val)
+            index_levelfs.append(val)
         
     if line.startswith('amc-general'): 
         temp = re.findall(r'\d+', line)
@@ -32,12 +34,12 @@ for line in data: # go through file line by line
         #Adding everything to dict
         d['User_levelfs'].append(key_user_levelfs)
         d['Inst_levelfs'].append(key_inst_levelfs)
+        d['Username'].append(val)
 
-
-df_levelfs = pd.DataFrame(d, index=index)
+df_levelfs = pd.DataFrame(d,)
 df_levelfs.to_html(open('df_levelfs.html', 'w'))
 
-print(f"********Parsing and Saving SUs as dataframe********\n")
+print(f"********Parsing and Saving levelfs as dataframe********\n")
 
 import matplotlib.pyplot as plt
 
@@ -47,8 +49,9 @@ levelfs=f"SU_{today_formatted}.txt"
 with open(levelfs, 'r') as f:
     data = f.readlines()
 
-d = { 'SU(%)': [], }
-index = []
+d = { 'Username': [],
+        'SU(%)': [], }
+index_SU = []
 
 for line in data: # go through file line by line
     if line.startswith('NEW USER'): #or line != 'LEVELFS\n': # skip new line characters
@@ -56,7 +59,7 @@ for line in data: # go through file line by line
         key, val = line.split(':', 1) # take the first 2 tokens from the split statement
 
         if key == 'NEW USER':
-            index.append(val)
+            index_SU.append(val)
 
 
     if line.startswith('alpine|amc-general'):
@@ -68,25 +71,43 @@ for line in data: # go through file line by line
 
         #Adding everything to dict
         d['SU(%)'].append(SU)
+        d['Username'].append(val)
 
-df_SU = pd.DataFrame(d, index=index)
+df_SU = pd.DataFrame(d,)
 df_SU.to_html(open('df_SU.html', 'w'))
 
+user_list_df_SU = []
+user_list_df_levelfs = []
+
+for ind,val in enumerate(index_levelfs):
+    mask = df_levelfs["Username"] == val
+    exec(f'df_levelfs_{ind}= df_levelfs[mask]')
+    exec(f'df_SU_{ind}= df_SU[mask]')
+    
+    # Saving individual dataframes:
+    exec(f'df_levelfs_{ind}.to_excel("levelfs_top_{val}_user.xlsx")')
+    exec(f'df_SU_{ind}.to_excel("SU_top_{val}_user.xlsx")')
+    
+    # Storing SUs and levelfs
+    exec(f'user_list_df_SU.append(df_SU_{ind})')    
+    exec(f'user_list_df_levelfs.append(df_levelfs_{ind})')
+
+
 print(f"********Plotting levelfs********\n")
-df_levelfs.plot.bar(y='User_levelfs', rot=0, figsize=(12, 8),);
+df_levelfs.plot.bar(y='User_levelfs', x = 'Username', rot=0, figsize=(12, 8),);
 plt.axhline(y=1, color='red', linestyle='dotted', linewidth=2.5)
 plt.xlabel('Users', fontsize=18)
-plt.ylabel('SUs (%)', fontsize=16)
+plt.ylabel('levelfs', fontsize=16)
 plt.xticks(rotation=45)
 plt.yticks(fontsize=20)
 plt.xticks(fontsize=20)
-plt.legend(["levelfs"], fontsize="20", loc ="upper right")
+plt.legend(["levelfs (coeff)"], fontsize="20", loc ="upper right")
 plt.title('User levelfs ' , fontsize=30)
 plt.savefig("Top_levelfs.png")
 plt.show()
 
 print(f"********Plotting SUs********\n")
-df_SU.plot.bar(y='SU(%)', rot=0, figsize=(12, 8),);
+df_SU.plot.bar(y='SU(%)', x = 'Username', rot=0, figsize=(12, 8),);
 plt.axhline(y=20, color='red', linestyle='dotted', linewidth=2.5)
 plt.xlabel('Users', fontsize=18)
 plt.ylabel('SUs (%)', fontsize=16)
@@ -97,3 +118,41 @@ plt.legend(["SU(%)"], fontsize="20", loc ="upper right")
 plt.title('Top 5 users percentage of SUs' , fontsize=30)
 plt.savefig("Top_SUs.png")
 plt.show()
+
+print(f"********Plotting individual levelfs and SUs********\n")
+
+
+for ind, df_ in enumerate(user_list_df_levelfs):
+    print(f"********Plotting levelfs********\n")
+    df_.plot.bar(y='User_levelfs', x = 'Username', rot=0, figsize=(12, 8),);
+    plt.axhline(y=1, color='red', linestyle='dotted', linewidth=2.5)
+    #plt.xlabel('Users', fontsize=18)
+    plt.ylabel('levelfs coeff', fontsize=16)
+    #plt.xticks(rotation=45)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.legend(["levelfs"], fontsize="20", loc ="upper right")
+    plt.title('User levelfs ' , fontsize=30)
+    levelfs=f"Top_user_levelfs_{index_levelfs[ind]}_.png"
+    plt.savefig(levelfs)
+    plt.show()
+
+
+for ind, df_ in enumerate(user_list_df_SU):
+    print(f"********Plotting SU********\n")
+    df_.plot.bar(y='SU(%)', x = 'Username', rot=0, figsize=(12, 8),);
+    plt.axhline(y=1, color='red', linestyle='dotted', linewidth=2.5)
+    #plt.xlabel('Users', fontsize=18)
+    plt.ylabel('SU (%)', fontsize=16)
+    #plt.xticks(rotation=45)
+    plt.yticks(fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.legend(["SU (%)"], fontsize="20", loc ="upper right")
+    plt.title('User SU ' , fontsize=30)
+    SU=f"Top_user_SUs_{index_SU[ind]}_.png"
+    plt.savefig(SU)
+    plt.show()
+
+
+print(f"********Done !!!!!!!!!********\n")
+
